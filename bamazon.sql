554a63e1a889d7e67d666180151e094f36d6f162
erin taylor <ewtaylor29@gmail.com>
2020-01-11 17:08:25 -0500

    price

--- bamazon.sql
+++ bamazon.sql
@@ -17,12 +17,12 @@ Select * FROM products;
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
 VALUES (101, "shirt", "clothing", 79.99, 20),
-	   (212, "football", "sports", 99.99, 10),
-	   (313, "bottle", "children", 29.99, 5),
+	   (212, "football", "sports", 19.99, 10),
+	   (313, "bottle", "children", 9.99, 5),
 	   (420, "earrings", "jewelry", 129.99, 14),
-	   (504, "milk", "grocery", 39.99, 15),
-	   (619, "pants", "clothing", 19.99, 19),
-	   (720, "bread", "grocery", 49.99, 11),
-	   (808, "diapers", "children", 69.99, 10),
-	   (913, "necklace", "jewelry", 9.99, 19),
-	   (1009, "tennis raquet", "sports", 89.99, 17)
+	   (504, "milk", "grocery", 3.99, 15),
+	   (619, "pants", "clothing", 89.99, 19),
+	   (720, "bread", "grocery", 2.99, 11),
+	   (808, "diapers", "children", 19.99, 10),
+	   (913, "necklace", "jewelry", 900.99, 19),
+	   (1009, "tennis raquet", "sports", 189.99, 17)
